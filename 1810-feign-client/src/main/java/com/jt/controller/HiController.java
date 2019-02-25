c95066265645809a9d5c798b97fb20fe64524de9
kdl <1150881012@qq.com>
2019-02-25 10:46:53 +0800

    答复

---
+++ 1810-feign-client/src/main/java/com/jt/controller/HiController.java
@@ -0,0 +1,20 @@
+package com.jt.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.jt.service.HiService;
+
+@Controller
+public class HiController {
+	@Autowired
+	private HiService hiService;
+	@RequestMapping("hi")
+	@ResponseBody
+	public String sayHi(String name){
+		String msg=hiService.sayHi(name);
+		return "FEIGN:"+msg;
+	}
+}
